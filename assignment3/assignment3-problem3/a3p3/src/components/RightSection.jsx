9d436d9c9907b6bea835af28ffc5c025238a891e
kishan7890 <kishansahu9348@gmail.com>
2024-05-27 11:53:33 +0530

    context api bulb on or off

---
+++ assignment3/assignment3-problem3/a3p3/src/components/RightSection.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { BulbContext } from "../context/BulbContextProvider";
+
+export default function RightSection() {
+  const { isOn, switchOn, switchOff } =
+    /*complete the missing code*/
+    useContext(BulbContext);
+
+
+  return (
+    <div className="right-section">
+      <p>Right Section ( Bulb ) </p>
+      <div className="light-bulb-container">
+        <div className={`light-bulb ${isOn ? "on" : "off"}`}></div>
+        <button
+          onClick={
+            /*complete the missing code*/
+            switchOn
+          }
+        >
+          SWITCH ON
+        </button>
+        <button
+          onClick={
+            /*complete the missing code*/
+            switchOff
+          }
+        >
+          SWITCH OFF
+        </button>
+      </div>
+    </div>
+  );
+}
